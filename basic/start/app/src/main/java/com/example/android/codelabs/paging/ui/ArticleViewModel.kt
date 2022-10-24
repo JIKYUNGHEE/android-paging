06076a9f408dee3d35413868dc05bb2ee187c56e
JIKYUNGHEE <wlrudgml517@gmail.com>
2022-10-25 08:19:38 +0900

    ViewModel에서 PagingData 요청 및 캐시

--- basic/start/app/src/main/java/com/example/android/codelabs/paging/ui/ArticleViewModel.kt
+++ basic/start/app/src/main/java/com/example/android/codelabs/paging/ui/ArticleViewModel.kt
@@ -18,11 +18,15 @@ package com.example.android.codelabs.paging.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.example.android.codelabs.paging.data.Article
 import com.example.android.codelabs.paging.data.ArticleRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.Flow
+
+private const val ITEMS_PER_PAGE = 50
 
 /**
  * ViewModel for the [ArticleActivity] screen.
@@ -35,10 +39,14 @@ class ArticleViewModel(
     /**
      * Stream of [Article]s for the UI.
      */
-    val items: StateFlow<List<Article>> = repository.articleStream
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
-            initialValue = listOf()
-        )
+    val items: Flow<PagingData<Article>> = Pager(
+        //로드 대기 시간, 초기 로드의 크기 요청 등 PagingSource에서 콘텐츠를 로드하는 방법에 관한 옵션을 설정
+        config = PagingConfig(pageSize = ITEMS_PER_PAGE, enablePlaceholders = false),
+                            //pageSize - 각 페이지에 로드해야 하는 항목 수 (필수 param)
+                            //사용자가 스크롤할 때 메모리를 낭비하지 않으려면 PagingConfig에서 maxSize 매개변수를 설정 (기본적으로 Paging은 로드하는 모든 페이지를 메모리에 유지)
+                            //enablePlaceholders - true인 경우 아직 로드되지 않은 콘텐츠의 자리표시자로 null 항목을 반환. 이렇게 하면 어댑터에 자리표시자 뷰를 표시할 수 있습니다.
+
+        pagingSourceFactory = { repository.articlePagingSource() }
+    ).flow
+        .cachedIn(viewModelScope)   //구성 또는 탐색 변경사항에도 페이징 상태를 유지(HOW? androidx.lifecycle.viewModelScope를 전달)
 }
